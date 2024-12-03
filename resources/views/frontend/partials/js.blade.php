983de9448d6912ab182f6a846f98320afb934a64
SkZubeirAli <zuber.nexgeno@gmail.com>
2024-12-03 18:33:55 +0530

    speed

--- resources/views/frontend/partials/js.blade.php
+++ resources/views/frontend/partials/js.blade.php
@@ -1,26 +1,26 @@
 <script type="text/javascript" src="/assets/frontend/js/jquery.js"></script>
-<script  defer type="text/javascript" src="/assets/frontend/js/owl-carousel.js"></script>
+<script  defer type="text/javascript" src="/assets/frontend/js/owl-carousel.js" defer></script>
 <script type="text/javascript" src="/assets/frontend/js/bootstrap5.min.js"></script>
-<script defer type="text/javascript" src="/assets/frontend/js/script.js"></script>
+<script defer type="text/javascript" src="/assets/frontend/js/script.js" defer></script>
 
 <!--moment js-->
-<script defer src="/assets/frontend/js/moment.min.js"></script>
+<script defer src="/assets/frontend/js/moment.min.js" defer></script>
 
 <!--jQuery Validate-->
 <script defer type="text/javascript" src="/assets/frontend/js/jquery.validate.min.js"></script>
 
 <!--select2-->
-<script defer type="text/javascript" src="/assets/frontend/js/select2.full.min.js"></script>
+<script defer type="text/javascript" src="/assets/frontend/js/select2.full.min.js" defer></script>
 
 <!--Toast Js-->
-<script defer type="text/javascript" src="/assets/frontend/js/toastr.min.js"></script>
+<script defer type="text/javascript" src="/assets/frontend/js/toastr.min.js" defer></script>
 
-<script defer type="text/javascript" src="/assets/frontend/js/Init.js"></script>
+<script defer type="text/javascript" src="/assets/frontend/js/Init.js" defer></script>
 
 <!-- <script defer src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> -->
 
 <script defer src="https://cdn.gtranslate.net/widgets/latest/dwf.js" defer></script>
-<script defer type="text/javascript" src="/assets/frontend/js/gtranslate_dwf.js"></script>
+<script defer type="text/javascript" src="/assets/frontend/js/gtranslate_dwf.js" defer></script>
 <!-- <script defer src="js/google_translate.js" defer></script> -->
 <!-- <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> -->
 <!-- <script type="text/javascript" src="/assets/frontend/js/google-translate.js"></script> -->
