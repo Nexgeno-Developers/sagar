aee1e9845dbe8b42d97249b083bf051128146804
SkZubeirAli <zuber.nexgeno@gmail.com>
2024-12-03 18:57:56 +0530

    add

--- resources/views/frontend/pages/home/index.blade.php
+++ resources/views/frontend/pages/home/index.blade.php
@@ -158,7 +158,7 @@ $cocs_pdf = '';
             <div class="carousel-inner">
                 @foreach ($banners as $index => $banner)
                 <div class="carousel-item @if($index==0) active @endif">
-                    <img src="{{ asset('storage/' . $banner->image) }}" class="d-block w-100 carousel-image" alt="...">
+                    <img src="{{ asset('storage/' . $banner->image) }}" loading="lazy" class="d-block w-100 carousel-image" alt="...">
                     <div class="carousel-caption banner_contents">
                         <h5>{{$banner->text}}</h5>
                         <a href="{{$banner->url}}" class="banner_view_button"
