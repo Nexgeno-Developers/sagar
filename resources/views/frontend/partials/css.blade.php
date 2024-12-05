7ee7eedb834ae5aa9098c5c6aede5794cf4ea15b
Mehtab645 <mehtab.makent@gmail.com>
2024-12-05 15:28:04 +0530

    12

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -7,7 +7,7 @@
 <link rel="shortcut icon" href="{{ asset('/assets/frontend/images/favicon.png') }}">
 
 <link rel="stylesheet" type="text/css" href="/assets/frontend/css/bootstrap.min.css" />
-<link rel="stylesheet" type="text/css" href="/assets/frontend/css/style.css?v=1.5.4" />
+<link rel="stylesheet" type="text/css" href="/assets/frontend/css/style.css?v=1.5.6" />
 
 <link rel="stylesheet" type="text/css" href="/assets/frontend/css/responsive.css?v=1.2.8"/>
 <link rel="stylesheet" type="text/css" href="/assets/frontend/css/toastr.min.css" media="print" onload="this.media='all'"/>
